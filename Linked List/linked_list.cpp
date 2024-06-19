0aa000051d369dee32f37c8dff74b97fe9a7f056
Saksham Joshi <sakshamjoshi555@gmail.com>
2024-06-19 18:18:26 +0100

    added linked list

--- Linked List/linked_list.cpp
+++ Linked List/linked_list.cpp
@@ -95,6 +95,62 @@ bool search(node* head, int key){
     return false;
 }
 
+void deleteAtHead(node* &head){
+    node *todelete = head;
+    head = head->next;
+
+    delete todelete;
+    return;
+}
+
+
+
+void deleteNode(node* &head, int val){
+    /**
+     * Deletes a node with a specific value from a linked list.
+     *
+     * @param head A reference to the head of the linked list.
+     * @param val The value of the node to be deleted.
+     *
+     * @return void
+     *
+     * @throws None
+     */
+
+    if(head == NULL){
+        cout<<"empty list"<<endl;
+        return;
+    }
+    if(head->next == NULL){
+        deleteAtHead(head);
+        return;
+    }
+
+    node *temp = head;
+    while(temp->next->data!=val){
+        temp = temp->next;
+    }
+    node *toDelete = temp->next;
+    temp->next = temp->next->next;
+    delete toDelete;
+    return;
+}
+
+node* reverse(node* &head){
+    node* prev = NULL;
+    node* curr = head;
+    node* nextptr;
+
+    while(curr!=NULL){
+        nextptr = curr->next;
+        curr->next = prev;
+
+        prev = curr;
+        curr = nextptr;
+    }
+    return prev;
+}
+
 int main(){
     node *head = NULL;
     insertAtTail(head,1);
@@ -103,8 +159,15 @@ int main(){
     insertAtTail(head,4);
     insertAtTail(head,5);
     display(head);
-    insertAthead(head,0);
-    display(head);
-    cout<<search(head, 5);
+    // insertAthead(head,0);
+    // display(head);
+    // cout<<search(head, 5);
+    // deleteNode(head, 3);
+    // display(head);
+    // deleteAtHead(head);
+    // display(head);
+
+    node* newHead = reverse(head);
+    display(newHead);
     return 0;
 }
